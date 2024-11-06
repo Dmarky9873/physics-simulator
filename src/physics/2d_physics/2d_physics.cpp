34c6b3800d0cbf8fad4cb4783198443bb20a9fec
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2024-11-06 16:31:08 -0500

    quick fix to prev commit

--- src/physics/2d_physics/2d_physics.cpp
+++ src/physics/2d_physics/2d_physics.cpp
@@ -1,24 +1,27 @@
 #include "2d_physics.h"
 
-TwoDPhysics::Velocity TwoDPhysics::KinematicEquations::calculate_final_velocity(Velocity initial_velocity, Acceleration acceleration, float time)
+TwoDPhysics::Velocity TwoDPhysics::KinematicEquations::calculate_final_velocity(const Velocity initial_velocity, const Acceleration acceleration, float time)
 {
     TwoDPhysics::Velocity final_velocity;
     TwoDPhysics::Acceleration acceleration_copy;
 
-    acceleration_copy.set_magnitude(acceleration.get_magnitude());
-    acceleration_copy.set_angle(acceleration.get_angle());
+    Acceleration a = acceleration;
+    Velocity v_i = initial_velocity;
+
+    acceleration_copy.set_magnitude(a.get_magnitude());
+    acceleration_copy.set_angle(a.get_angle());
 
     acceleration_copy.multiply_scalar(time);
 
-    final_velocity.set_magnitude(initial_velocity.get_magnitude());
-    final_velocity.set_angle(initial_velocity.get_angle());
+    final_velocity.set_magnitude(v_i.get_magnitude());
+    final_velocity.set_angle(v_i.get_angle());
 
     final_velocity.add_vector2(acceleration_copy);
 
     return final_velocity;
 }
 
-TwoDPhysics::Displacement TwoDPhysics::KinematicEquations::calculate_displacement(Velocity initial_velocity, Acceleration acceleration, float time)
+TwoDPhysics::Displacement TwoDPhysics::KinematicEquations::calculate_displacement(const Velocity initial_velocity, const Acceleration acceleration, float time)
 {
     TwoDPhysics::Displacement displacement;
 
